12b94f4eb1cc0c8fd4cdc3c103e1c9d2568c39b5
Justin Laureano <jmlaureano87@gmail.com>
2024-08-16 13:35:41 -0400

    Added initial language files

---
+++ lang/en/frontend.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Frontend Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines contain the frontend content shown to users. 
+    |
+    */
+
+    'username' => 'Username',
+];
